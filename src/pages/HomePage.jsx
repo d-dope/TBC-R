3a0bdea3e5145a00cf56e4196f06337105ffa875
Davit <davitsulkhanishvili18@gmail.com>
2024-03-25 15:47:06 +0400

    Installed React Router and added Routes

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,23 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import HeroSection from "../components/HeroSection";
+import Search from "../components/Search";
+
+function HomePage() {
+    
+
+    return (
+        <>
+        <div className="App bg-white-200 min-h-screen overflow-hidden">
+            
+             <Header/>
+            <Search/>
+            <HeroSection/>
+            <Footer/> 
+       
+      </div>
+        </>
+    )
+}
+
+export default HomePage;
