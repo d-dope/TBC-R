9ee8ce82557d1b60ca006b1daaa5410a5a924c7e
d-dope <davitsulkhanishvili18@gmail.com>
2024-04-28 15:14:45 +0400

    Added Internationalization

--- src/app/[locale]/(dashboard)/blogs/page.js
+++ src/app/[locale]/(dashboard)/blogs/page.js
@@ -1,10 +1,12 @@
 import BlogList from "@/app/components/BlogList";
+import { useTranslations } from "next-intl";
 
 export default function Blogs() {
+  const t = useTranslations('Blogs')
   return (
     <>
       <section id="home" className="flex justify-center mt-7">
-        <h2 className="text-3xl font-bold">Blogs</h2>
+        <h2 className="text-3xl font-bold">{t('Blogs')}</h2>
       </section>
       <div className="">
         <BlogList />
--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,41 +1,19 @@
-"use client";
 import image6 from "../../../public/assets/6Instagram_icon.png.webp";
 import image7 from "../../../public/assets/7Facebook-Logosu.png";
 import image8 from "../../../public/assets/8LinkedIn_logo_initials.png";
 import image9 from "../../../public/assets/9.jpg";
 import Link from "next/link";
 import Image from "next/image";
+import { useTranslations } from "next-intl";
+import Navigation from "./Navigation";
 export default function Footer() {
+  const t = useTranslations('Navigation')
   return (
     <>
       <footer className="bg-gradient-to-r from-sky-500 to-indigo-500 py-4 flex items-center mt-2">
         <div className="container mx-auto">
           <ul className="flex mb-8 justify-center gap-20 mt-0">
-            <li className="mr-4">
-              <Link href="/" className="text-white hover:text-gray-300">
-                Home
-              </Link>
-            </li>
-            <li className="mr-4">
-              <Link href="/blogs" className="text-white hover:text-gray-300">
-                Blogs
-              </Link>
-            </li>
-            <li className="mr-4">
-              <Link href="/services" className="text-white hover:text-gray-300">
-                Services
-              </Link>
-            </li>
-            <li>
-              <Link href="/contact" className="text-white hover:text-gray-300">
-                Contact
-              </Link>
-            </li>
-            <li>
-              <Link href="/profile" className="text-white hover:text-gray-300">
-                Profile
-              </Link>
-            </li>
+              <Navigation/>
           </ul>
           <div className="flex justify-between items-center px-24 ">
             <ul className="flex">
