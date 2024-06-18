0052ad822538f5380094c2d88f90af88f55343a1
Davit <davitsulkhanishvili18@gmail.com>
2024-06-18 16:54:49 +0400

    Added feature that only admin can delete product

---
+++ src/app/components/DeleteCartBtn.tsx
@@ -0,0 +1,22 @@
+import { deleteSingleProduct } from "../../../actions";
+
+interface DeleteCartBtnProps {
+  id: number;
+}
+
+export default function DeleteCartBtn({ id }: DeleteCartBtnProps) {
+  const handleDelete = async () => {
+    await deleteSingleProduct(id);
+  };
+  
+  return (
+    <div>
+      <button
+        onClick={handleDelete}
+        className="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600"
+      >
+        Remove
+      </button>
+    </div>
+  );
+}
