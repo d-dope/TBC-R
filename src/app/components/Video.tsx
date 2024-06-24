917eba197619cbfe398904608fa234b4b75b2532
Davit <davitsulkhanishvili18@gmail.com>
2024-06-24 18:50:18 +0400

    Added video on landing

--- src/app/components/Video.tsx
+++ src/app/components/Video.tsx
@@ -1,14 +1,22 @@
+import React from "react";
+
 const Video = () => {
-    const videoUrl =
-      "https://live.staticflickr.com/video/53808141483/834ef8b783/1080p.mp4?s=eyJpIjo1MzgwODE0MTQ4MywiZSI6MTcxOTA2MDAyMywicyI6ImYyOWY0YTNlYzRiMTEyZDE0Nzg0YTg2MmYzNGRiMjI3YzE4N2YwN2EiLCJ2IjoxfQ";
-    return (
-      <div className="w-full h-[550px] overflow-hidden">
-        <div className="h-full w-full">
-          <video className="h-full w-full object-cover" src={videoUrl} autoPlay muted loop />
-        </div>
+  const videoUrl =
+    "https://1mwujqq5q9dsefex.public.blob.vercel-storage.com/LandingVideo%20(online-video-cutter.com)%20(1)-EcDCxFCt1wlkBORoQTfaHbRefZ9WtY.mp4";
+
+  return (
+    <div className="w-full h-[550px] overflow-hidden">
+      <div className="h-full w-full">
+        <video
+          className="h-full w-full object-cover filter brightness-50"
+          src={videoUrl}
+          autoPlay
+          muted
+          loop
+        />
       </div>
-    );
-  };
-  
-  export { Video };
-  
+    </div>
+  );
+};
+
+export { Video };
