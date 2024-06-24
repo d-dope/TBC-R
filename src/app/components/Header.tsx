ddebb43636bec1d437d59599ed8c64334dc84a31
Davit <davitsulkhanishvili18@gmail.com>
2024-06-24 19:03:27 +0400

    Started SEO

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -86,7 +86,7 @@ export default function Header() {
                       key={`item-generate-${item.name}`}
                       href={item.href}
                       className={classNames(
-                        "inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium font-sans",
+                        "inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium",
                         currentPath === item.href
                           ? "border-primaryColor text-gray-900"
                           : "border-transparent text-gray-900 hover:border-gray-300 hover:text-gray-700"
