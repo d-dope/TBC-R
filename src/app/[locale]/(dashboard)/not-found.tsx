4470d532999ddcae1e3e0ab055aeada3637c2df2
Davit <davitsulkhanishvili18@gmail.com>
2024-06-25 02:51:11 +0400

    added 404 style

--- src/app/[locale]/(dashboard)/not-found.tsx
+++ src/app/[locale]/(dashboard)/not-found.tsx
@@ -1,33 +1,32 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <>
-      {/*
-          This example requires updating your template:
-  
-          ```
-          <html class="h-full">
-          <body class="h-full">
-          ```
-        */}
-      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <main className="grid min-h-full place-items-center bg-white dark:bg-black px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
-          <p className="text-base font-semibold text-primaryColor mt-32">404</p>
-          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+          <p className="text-base font-semibold text-primaryColor mt-32 dark:text-gray-300">
+            404
+          </p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-5xl">
             Page not found
           </h1>
-          <p className="mt-6 text-base leading-7 text-gray-600">
+          <p className="mt-6 text-base leading-7 text-gray-600 dark:text-gray-400">
             Sorry, we couldn’t find the page you’re looking for.
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
               href="#"
-              className="rounded-md bg-primaryColor px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:opacity-85 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="rounded-md bg-primaryColor px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:opacity-85 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-gray-800 dark:hover:opacity-90 dark:focus-visible:outline-indigo-400"
             >
               Go back home
             </a>
-            <a href="#" className="text-sm font-semibold text-gray-900">
+            <Link
+              href="/contact"
+              className="text-sm font-semibold text-gray-900 dark:text-gray-300"
+            >
               Contact support <span aria-hidden="true">&rarr;</span>
-            </a>
+            </Link>
           </div>
         </div>
       </main>
