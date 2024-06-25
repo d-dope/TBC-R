a3e1ee49d7cdb8ab3e01e88bd13ed73a8da5e291
Davit <davitsulkhanishvili18@gmail.com>
2024-06-25 16:19:41 +0400

    Added markup

--- src/app/components/DeleteBlogBtn.tsx
+++ src/app/components/DeleteBlogBtn.tsx
@@ -28,7 +28,7 @@ export default function DeleteBlogBtn({ id }: DeleteCartBtnProps) {
     <div>
       <button
         onClick={handleDelete}
-        className="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600"
+        className="w-full bg-gray-200 mt-10 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600"
       >
         Remove
       </button>
--- src/app/components/SingleBlogView.tsx
+++ src/app/components/SingleBlogView.tsx
@@ -32,14 +32,14 @@ export default function SingleBlogView({ blog }: SingleBlogViewProps) {
   const shareUrl = `https://tbc-r.vercel.app/blogs/${blog.id}`;
   const title = blog.title;
   return (
-    <div className="bg-gray-100 dark:bg-black py-32 ">
+    <div className="bg-gray-100 dark:bg-black py-32  ">
       <div className="max-w-6xl mx-auto mt-32 px-4 sm:px-6 lg:px-8">
-        <div className="flex flex-col md:flex-row -mx-4 ">
+        <div className="flex flex-col md:flex-row -mx-4  ">
           <div className="md:flex-1 px-4 flex flex-col justify-between">
-            <div className="sm:h-[300px] h-[200px] rounded-lg dark:bg-gray-700 mb-4">
+            <div className="sm:h-[300px] h-[200px] rounded-lg dark:bg-black mb-4">
               <Zoom>
                 <Image
-                  className="w-full h-full object-cover"
+                  className=" w-full h-80 object-cover rounded "
                   height={460}
                   width={460}
                   src={blog.picture_url}
